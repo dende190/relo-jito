5dfba1f43628604b106b18b8a0b3e0bbf2688945
Piyin <piyinternet@outlook.com>
2022-10-03 17:12:23 -0500

    Quita código que parece no ser necesario

--- main.js
+++ main.js
@@ -57,8 +57,4 @@ app.on('window-all-closed', () => {
 
 app.whenReady().then(() => {
   createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  });
 });
